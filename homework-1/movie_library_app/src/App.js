0ce13f7294696cab7a54ab0c7ce0ca9aa0fba752
Ivan Arsovski <117305795+arsovski96@users.noreply.github.com>
2023-06-06 22:22:09 +0200

    Added code from homework

---
+++ homework-1/movie_library_app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MoviesList from "./components/MoviesList.js";
+
+function App() {
+  return (
+    <div className="App">
+      <MoviesList />
+    </div>
+  );
+}
+
+export default App;
